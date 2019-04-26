f9367766c0399f44999338696cef9278a91c91ad
ivica petrsoric <ivica.petrsoric@codecacao.com>
2019-04-26 18:44:51 +0200

    Introduction: Setting Up UI: Smart Refactoring Patterns

---
+++ LikeAppStore/Controller/AppSearchController.swift
@@ -0,0 +1,27 @@
+//
+//  AppSearchController.swift
+//  LikeAppStore
+//
+//  Created by ivica petrsoric on 26/04/2019.
+//  Copyright © 2019 ivica petrsoric. All rights reserved.
+//
+
+import UIKit
+
+class AppSearchController: UICollectionViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        collectionView.backgroundColor = .white
+    }
+    
+    init() {
+        super.init(collectionViewLayout: UICollectionViewFlowLayout())
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
---
+++ LikeAppStore/Controller/BaseTabBarController.swift
@@ -0,0 +1,33 @@
+//
+//  BaseTabBarController.swift
+//  LikeAppStore
+//
+//  Created by ivica petrsoric on 26/04/2019.
+//  Copyright © 2019 ivica petrsoric. All rights reserved.
+//
+
+import UIKit
+
+class BaseTabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        viewControllers = [
+            createNavController(viewController: UIViewController(), title: "Today", imageName: "today_icon"),
+            createNavController(viewController: UIViewController(), title: "Apps", imageName: "apps"),
+            createNavController(viewController: AppSearchController(), title: "Search", imageName: "search")
+        ]
+    }
+    
+    private func createNavController(viewController: UIViewController, title: String, imageName: String) -> UIViewController {
+        let navController = UINavigationController(rootViewController: viewController)
+        navController.tabBarItem.title = title
+        navController.navigationBar.prefersLargeTitles = true
+        navController.tabBarItem.image = UIImage(named: imageName)
+        viewController.view.backgroundColor = .white
+        viewController.navigationItem.title = title
+        return navController
+    }
+    
+}
