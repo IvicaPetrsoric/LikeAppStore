eedee09970a1e1b74f1c92677d111482382503b2
ivica petrsoric <ivica.petrsoric@codecacao.com>
2019-04-26 19:54:07 +0200

    Introduction: Setting Up UI: Cells and UIStackView Layouts

--- LikeAppStore/Controller/AppSearchController.swift
+++ LikeAppStore/Controller/AppSearchController.swift
@@ -8,12 +8,29 @@
 
 import UIKit
 
-class AppSearchController: UICollectionViewController {
+class AppSearchController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+    
+    private let cellId = "cellId"
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         collectionView.backgroundColor = .white
+        
+        collectionView.register(SearchResultCell.self, forCellWithReuseIdentifier: cellId)
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! SearchResultCell
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width, height: 250)
     }
     
     init() {
---
+++ LikeAppStore/View/SearchResultCell.swift
@@ -0,0 +1,77 @@
+//
+//  SearchResultCell.swift
+//  LikeAppStore
+//
+//  Created by ivica petrsoric on 26/04/2019.
+//  Copyright © 2019 ivica petrsoric. All rights reserved.
+//
+
+import UIKit
+
+class SearchResultCell: UICollectionViewCell {
+    
+    let imageView: UIImageView = {
+        let iv = UIImageView()
+        iv.backgroundColor = .red
+        iv.widthAnchor.constraint(equalToConstant: 64).isActive = true
+        iv.heightAnchor.constraint(equalToConstant: 64).isActive = true
+        iv.layer.cornerRadius = 12
+        return iv
+    }()
+    
+    let nameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "App name"
+        return label
+    }()
+    
+    let categoryLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Photos & videos"
+        return label
+    }()
+    
+    let ratingLabel: UILabel = {
+        let label = UILabel()
+        label.text = "9.26M"
+        return label
+    }()
+    
+    let getButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("GET", for: .normal)
+        button.setTitleColor(.blue, for: .normal)
+        button.titleLabel?.font = .boldSystemFont(ofSize: 14)
+        button.backgroundColor = .darkGray
+        button.widthAnchor.constraint(equalToConstant: 80).isActive = true
+        return button
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        let labelsStackView = UIStackView(arrangedSubviews: [
+            nameLabel, categoryLabel,ratingLabel
+            ])
+        labelsStackView.axis = .vertical
+        
+        let stackView = UIStackView(arrangedSubviews: [
+            imageView, labelsStackView, getButton
+            ])
+        
+        stackView.spacing = 12
+        stackView.alignment = .center
+        
+        addSubview(stackView)
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.topAnchor.constraint(equalTo: topAnchor).isActive = true
+        stackView.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
+        stackView.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
+        stackView.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
