d8ccc73fe13198faf2f0e348d42335aadae70afd
ivica petrsoric <ivica.petrsoric@codecacao.com>
2019-06-11 19:25:58 +0200

    Today Feed; Today Controller Cells

---
+++ LikeAppStore/Controller/Today/TodayController.swift
@@ -0,0 +1,49 @@
+//
+//  TodayController.swift
+//  LikeAppStore
+//
+//  Created by ivica petrsoric on 11/06/2019.
+//  Copyright © 2019 ivica petrsoric. All rights reserved.
+//
+
+import UIKit
+
+class TodayController: BaseListController, UICollectionViewDelegateFlowLayout {
+    
+    private let cellId = "cellId"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        collectionView.backgroundColor = #colorLiteral(red: 0.9520952106, green: 0.9459629655, blue: 0.9522011876, alpha: 1)
+        collectionView.register(TodayCell.self, forCellWithReuseIdentifier: cellId)
+        
+        navigationController?.isNavigationBarHidden = true
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! TodayCell
+        return cell
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print("animate full screen")
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return .init(width: view.frame.width - 64, height: 450)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 32
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return .init(top: 32, left: 0, bottom: 32, right: 0)
+    }
+    
+}
---
+++ LikeAppStore/Views/Today/TodayCell.swift
@@ -0,0 +1,33 @@
+//
+//  TodayCell.swift
+//  LikeAppStore
+//
+//  Created by ivica petrsoric on 11/06/2019.
+//  Copyright © 2019 ivica petrsoric. All rights reserved.
+//
+
+import UIKit
+
+class TodayCell: UICollectionViewCell {
+    
+    let imageView = UIImageView(image: #imageLiteral(resourceName: "garden"))
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        backgroundColor = .white
+        
+        layer.cornerRadius = 16
+        clipsToBounds = true
+        
+        imageView.contentMode = .scaleAspectFill
+        
+        addSubview(imageView)
+        imageView.centerInSuperview(size: .init(width: 250, height: 250))
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
