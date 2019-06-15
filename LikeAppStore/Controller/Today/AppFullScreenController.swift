35c861130f425d8eb29b1090f0d5c9ebf6e8d1f6
ivica petrsoric <ivica.petrsoric@codecacao.com>
2019-06-15 14:29:26 +0200

    Bonus Lessons: Interesting Details: Floating Controls View

--- LikeAppStore/Controller/Today/AppFullScreenController.swift
+++ LikeAppStore/Controller/Today/AppFullScreenController.swift
@@ -40,7 +40,51 @@ class AppFullScreenController: UIViewController, UITableViewDataSource, UITableV
         tableView.contentInsetAdjustmentBehavior = .never
         let height = UIApplication.shared.statusBarFrame.height
         tableView.contentInset = .init(top: 0, left: 0, bottom: height, right: 0)
+     
+        setupFloatingControls()
+    }
+    
+    private func setupFloatingControls() {
+        let floatingContainerView = UIView()
+        floatingContainerView.clipsToBounds = true
+        floatingContainerView.layer.cornerRadius = 16
+
+        view.addSubview(floatingContainerView)
+        let bottomPadding = UIApplication.shared.statusBarFrame.height
+        floatingContainerView.anchor(top: nil, leading: view.leadingAnchor,
+                                     bottom: view.bottomAnchor, trailing: view.trailingAnchor,
+                                     padding: .init(top: 0, left: 16, bottom: bottomPadding, right: 16),
+                                     size: .init(width: 0, height: 100))
         
+        let blurVisualEffectView = UIVisualEffectView(effect: UIBlurEffect(style: .regular))
+        floatingContainerView.addSubview(blurVisualEffectView)
+        blurVisualEffectView.fillSuperview()
+        
+        let imageView = UIImageView(cornerRadius: 16)
+        imageView.image = todayItem?.image
+        imageView.constrainHeight(constant: 68)
+        imageView.constrainWidth(constant: 68)
+        
+        let getButton = UIButton(title: "GET")
+        getButton.setTitleColor(.white, for: .normal)
+        getButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 16)
+        getButton.backgroundColor = .darkGray
+        getButton.layer.cornerRadius = 16
+        getButton.constrainWidth(constant: 80)
+        getButton.constrainHeight(constant: 32)
+        
+        let stackView = UIStackView(arrangedSubviews: [
+            imageView,
+            VerticalStackView(arrangedSubviews: [
+                UILabel(text: "Life hack", font: .boldSystemFont(ofSize: 18)),
+                UILabel(text: "Utilizing your time", font: .systemFont(ofSize: 16)),
+                ], spacing: 4),
+            getButton
+            ], customSpacing: 16)
+        
+        floatingContainerView.addSubview(stackView)
+        stackView.fillSuperview(padding: .init(top: 0, left: 16, bottom: 0, right: 16))
+        stackView.alignment = .center
     }
     
     private func setuoCloseButton() {
