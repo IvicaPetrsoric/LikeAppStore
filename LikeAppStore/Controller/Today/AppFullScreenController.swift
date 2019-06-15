7fd54052e1e1f61c345c8cce5cb5f76741b28152
ivica petrsoric <ivica.petrsoric@codecacao.com>
2019-06-15 14:45:48 +0200

    Bonus Lessons: Interesting Details: UIView Transform Animations

--- LikeAppStore/Controller/Today/AppFullScreenController.swift
+++ LikeAppStore/Controller/Today/AppFullScreenController.swift
@@ -44,17 +44,17 @@ class AppFullScreenController: UIViewController, UITableViewDataSource, UITableV
         setupFloatingControls()
     }
     
+    private let floatingContainerView = UIView()
+
     private func setupFloatingControls() {
-        let floatingContainerView = UIView()
         floatingContainerView.clipsToBounds = true
         floatingContainerView.layer.cornerRadius = 16
 
         view.addSubview(floatingContainerView)
-        let bottomPadding = UIApplication.shared.statusBarFrame.height
         floatingContainerView.anchor(top: nil, leading: view.leadingAnchor,
                                      bottom: view.bottomAnchor, trailing: view.trailingAnchor,
-                                     padding: .init(top: 0, left: 16, bottom: bottomPadding, right: 16),
-                                     size: .init(width: 0, height: 100))
+                                     padding: .init(top: 0, left: 16, bottom: -90, right: 16),
+                                     size: .init(width: 0, height: 90))
         
         let blurVisualEffectView = UIVisualEffectView(effect: UIBlurEffect(style: .regular))
         floatingContainerView.addSubview(blurVisualEffectView)
@@ -100,6 +100,13 @@ class AppFullScreenController: UIViewController, UITableViewDataSource, UITableV
             scrollView.isScrollEnabled = false
             scrollView.isScrollEnabled = true
         }
+        
+        let translationY = -90 - UIApplication.shared.statusBarFrame.height
+        let transform = scrollView.contentOffset.y > 100 ? CGAffineTransform(translationX: 0, y: translationY) : .identity
+        
+        UIView.animate(withDuration: 0.7, delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 0.7, options: .curveEaseOut, animations: {
+            self.floatingContainerView.transform = transform
+        })
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
